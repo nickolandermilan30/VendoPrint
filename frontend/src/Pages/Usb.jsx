8f5286718167f9725f2972a981c5874f4a3b2307
Jowen <diezjowenmiguel@gmail.com>
2025-03-18 20:34:15 +0800

    try

--- frontend/src/Pages/Usb.jsx
+++ frontend/src/Pages/Usb.jsx
@@ -302,7 +302,7 @@ const Usb = () => {
           printerName: selectedPrinter,
           fileUrl: finalFileUrlToPrint,
           copies: copies,
-          isColor: isColor,
+          
         });
 
         if (response.data.success) {
