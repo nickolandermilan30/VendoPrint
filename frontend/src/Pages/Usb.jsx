4b1c3632d24b65af4a14bc6e037d170edc06a039
Jowen <diezjowenmiguel@gmail.com>
2025-03-19 23:36:22 +0800

    try

--- frontend/src/Pages/Usb.jsx
+++ frontend/src/Pages/Usb.jsx
@@ -191,7 +191,7 @@ const Usb = () => {
     let finalFileUrlToPrint = filePreviewUrl;
   
     try {
-      if (fileType?.type === "application/pdf") {
+      if (fileType === "application/pdf") {
         const existingPdfBytes = await fetch(filePreviewUrl).then((res) =>
           res.arrayBuffer()
         );
@@ -235,7 +235,7 @@ const Usb = () => {
       } 
   
       else if (
-        fileType?.type ===
+        fileType ===
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
       ) {
         const arrayBuffer = await fetch(filePreviewUrl).then((res) =>
